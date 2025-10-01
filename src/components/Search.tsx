dd7e2dc6cbd14010ce6263e511edf8052fce9236
conormcfarlane <conormcf98@gmail.com>
2025-10-01 19:56:31 +1000

    API and search bar working

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,23 +1,37 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import iconSearch from '/images/icon-search.svg'
+import useWeatherApi from '../hooks/useWeatherApi'
+
 export default function Search() {
-  const [city,setCity] = useState("")
-  const handleSearch = () =>{
-    
+
+  const [input,setInput] = useState<string>("");
+  const [city,setCity] = useState<string>("");
+    const {data,loading,error} = useWeatherApi(city)
+  const handleSearch = (e: React.FormEvent) =>{
+    e.preventDefault();
+    setCity(input);
   }
+
   return (
-    <section className=' flex flex-col gap-3'>
+    <section>
+      <form className='flex flex-col gap-3' onSubmit={handleSearch}>
         <div className='bg-neutral-800 flex px-6 py-4 gap-4 items-center rounded-xl'>
-         <img src={iconSearch} alt="search icon" className='h-fit' />
-        <input type="text" 
-        placeholder='Search for a place...'
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        className='text-xl leading-[120%] text-neutral-200 cursor-pointer outline-none'/>
+          <img src={iconSearch} alt="search icon" className='h-fit' />
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder='Search for a place...'
+            className='text-xl leading-[120%] text-neutral-200 cursor-pointer outline-none'
+          />
         </div>
-        <button onClick={handleSubmit} className='bg-blue-500 text-white text-xl leading-[120%] py-4 rounded-xl'>
-            Search
+        <button className='bg-blue-500 text-white text-xl leading-[120%] py-4 rounded-xl'>
+          Search
         </button>
+       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      </form>
     </section>
   )
 }
+
+
--- src/hooks/useWeatherApi.ts
+++ src/hooks/useWeatherApi.ts
@@ -1,18 +1,39 @@
-import React from "react";
-import { useState,useEffect } from "react";
+import { useState,useEffect} from "react";
 
 function useWeatherApi(query: string){
-    const [data,setData] = useState<any>(null);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error,setError] = useState<string | null >(null);
-
+    const [data,setData] = useState<any>("")
+    const [loading,setLoading] = useState<boolean>(true);
+    const [error,setError] = useState<string | null >("");
 
     useEffect(() => {
-        if(!query) return;
         const fetchWeather = async () => {
-            
+            setLoading(true)
+            setError(null)
+
+            try{
+                // fetch geo lat and longititude
+                const geoRes = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(query)}`)
+                const geoData = await geoRes.json();
+                if(!geoData.results || geoData.results.length === 0){
+                    setError("Location not found")
+                    setLoading(false);
+                    return;
+                }
+
+                // 2 get weather using lat and long 
+                const {latitude,longitude} = geoData.results[0]
+                const weatherRes = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`)
+                const weatherData = await weatherRes.json();
+                setData(weatherData);
+            }catch{
+                setError("Failed to retireve weather data")
+            }finally{
+                setLoading(false)
+            }
         }
-    })
-
-
+        fetchWeather()
+    },[query]);
+    return {data,loading,error};
 }
+
+export default useWeatherApi;
